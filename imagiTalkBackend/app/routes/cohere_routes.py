dbe39209da8d286587d58d6d61c3e7f8aeb547cd
AryanK1511 <aryankhurana1511@gmail.com>
2024-01-27 07:28:05 -0500

    Fixed compatibility issue

--- imagiTalkBackend/app/routes/cohere_routes.py
+++ imagiTalkBackend/app/routes/cohere_routes.py
@@ -1,22 +1,22 @@
-from app import app
-import cohere
-import os
-from dotenv import load_dotenv
-from flask import request, jsonify
+# from app import app
+# import cohere
+# import os
+# from dotenv import load_dotenv
+# from flask import request, jsonify
 
-# Configure environment variables
-load_dotenv()
+# # Configure environment variables
+# load_dotenv()
 
-cohere_key = os.getenv("COHERE_KEY")
-co = cohere.Client(cohere_key)
+# cohere_key = os.getenv("COHERE_KEY")
+# co = cohere.Client(cohere_key)
 
-# ========== API ROUTE TO GET COHERE TEXT GENERATION (BETA) ==========
-@app.route('/api/cohere/generate', methods=['POST'])
-def generate_text():
-    user_prompt = request.json
+# # ========== API ROUTE TO GET COHERE TEXT GENERATION (BETA) ==========
+# @app.route('/api/cohere/generate', methods=['POST'])
+# def generate_text():
+#     user_prompt = request.json
 
-    # Generate text using cohere API
-    response = co.generate(prompt=user_prompt["data"])
+#     # Generate text using cohere API
+#     response = co.generate(prompt=user_prompt["data"])
     
-    return jsonify({"result": response_str})
+#     return jsonify({"result": response_str})
 
