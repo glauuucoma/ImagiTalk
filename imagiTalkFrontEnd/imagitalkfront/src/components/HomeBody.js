16e5c2b119b8f389b19f796e9da4f7bd984d7e1a
Arina Kolodeznikova <arinak1017@gmail.com>
2024-01-27 09:31:10 -0500

    Home Page Basic Styling.

--- imagiTalkFrontEnd/imagitalkfront/src/components/HomeBody.js
+++ imagiTalkFrontEnd/imagitalkfront/src/components/HomeBody.js
@@ -5,17 +5,13 @@ const HomeBody = () => {
         <Container fluid className="m-5 home-body">
             <Row className="home-body-text-block">
                 <Col md={7} className="pl-5">
-                    <h1 className="custom-header">Chat with you favourite character from the past</h1>
-                    <p>Just type out the name of the character you want to talk to and explore the endless possibilities of interactive storytelling</p>
+                    <h1 className="custom-header">Chat with your</h1> 
+                    <h1 className="custom-header">favourite character</h1>
+                    <h1 className="custom-header">from the past</h1>
+                    <p>Just type out the name of the character you want to <br/> talk to and explore the endless possibilities of<br/> interactive storytelling</p>
+                    <Button className="start-chatting-btn px-5 mt-2">CHAT</Button> 
                 </Col>
             </Row>  
-            <Row>
-                <Col>
-                    <div className='btn-div'>
-                        <Button className="start-chatting-btn">CHAT</Button>
-                    </div>  
-                </Col>
-            </Row>
         </Container>
     </>)
 }
