122d23d2df6950a70f273b1600c5e819df3b8d35
AryanK1511 <aryankhurana1511@gmail.com>
2024-01-27 05:33:23 -0500

    Updated Dockerfile

--- imagiTalkBackend/app/config.py
+++ imagiTalkBackend/app/config.py
@@ -5,10 +5,3 @@ class Config:
     POSTGRES_DB_USERNAME=os.environ.get('POSTGRES_DB_USERNAME') or None
     POSTGRES_DB_PASSWORD=os.environ.get('POSTGRES_DB_PASSWORD') or None
     SQLALCHEMY_DATABASE_URI = f'postgresql://{POSTGRES_DB_USERNAME}:{POSTGRES_DB_PASSWORD}@kashin.db.elephantsql.com/cpvcyipo'
-
-    # ===== AUTH0 CONFIG =====
-    # SERVER_NAME = '2452-192-12-181-28.ngrok-free.app'
-    # PREFERRED_URL_SCHEME = 'https'
-
-    # Debug mode (set to True for development, False for production)
-    DEBUG = True
