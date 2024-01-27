74aadce832c92002e998d7fa91c36f441e8a1556
AryanK1511 <aryankhurana1511@gmail.com>
2024-01-27 08:30:35 -0500

    Added DB changes

--- imagiTalkBackend/app/routes/cohere_routes.py
+++ imagiTalkBackend/app/routes/cohere_routes.py
@@ -1,22 +1,26 @@
-# from app import app
-# import cohere
-# import os
-# from dotenv import load_dotenv
-# from flask import request, jsonify
+from app import app
+import cohere
+import os
+from dotenv import load_dotenv
+from flask import request, jsonify
 
-# # Configure environment variables
-# load_dotenv()
+# Configure environment variables
+load_dotenv()
 
-# cohere_key = os.getenv("COHERE_KEY")
-# co = cohere.Client(cohere_key)
+cohere_key = os.getenv("COHERE_KEY")
+print("Key: ", cohere_key)
+co = cohere.Client(cohere_key)
 
-# # ========== API ROUTE TO GET COHERE TEXT GENERATION (BETA) ==========
-# @app.route('/api/cohere/generate', methods=['POST'])
-# def generate_text():
-#     user_prompt = request.json
+# ========== API ROUTE TO GET COHERE TEXT GENERATION (BETA) ==========
+@app.route('/api/cohere/generate', methods=['POST'])
+def generate_text():
+    user_prompt = request.json
 
-#     # Generate text using cohere API
-#     response = co.generate(prompt=user_prompt["data"])
-    
-#     return jsonify({"result": response_str})
+    # Generate text using cohere API
+    response = co.generate(prompt=user_prompt["data"])
+
+    generated_text = response.generations[0].text
+
+    # Return the generated text in a JSON-serializable format
+    return jsonify({"result": generated_text})
 
